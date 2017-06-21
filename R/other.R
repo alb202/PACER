b43846d26790eae3e8b5c80c90ba2690ec1dbd28
Andrew Blumenfeld <ablumenf@gmail.com>
2017-06-20 18:44:07 -0600

    Committing again, not sure if everything was included

--- R/other.R
+++ R/other.R
@@ -39,22 +39,6 @@ invert_vector <- function(x){
   return(x)
 }
 
-shuffle_intervals <- function(alignments, genome_sizes, n=NULL, regions=NULL, invert=FALSE, seed=0, stranded=TRUE){
-
-  strands <- c("+", "-")
-
-  if (is.null(n))
-    n <- length(alignments)
-  results <- ""
-  for (i in 1:n){
-
-    makeGRangesFromDataFrame(data.frame(seqnames="I", start="38482", end="94998", strand="+"))
-    a <- c(a,makeGRangesFromDataFrame(data.frame(seqnames="IV", start="438482", end="594998", strand="-")))
-  }
-
-  return(sort.GenomicRanges(results))
-}
-
 swap_values <- function(x, old, new){
   tmp <- list()
   if (length(old)!= length(new))
@@ -67,3 +51,76 @@ swap_values <- function(x, old, new){
   }
   return(x)
 }
+
+
+
+shuffle_intervals <- function(alignments, chromosome_sizes, sample=NULL, include=NULL, seed=0, stranded=TRUE, chrom=TRUE){
+  #eval(as.name("II"))
+  strands <- c("+", "-")
+  chromosomes <- levels(chromosome_sizes$chrom)
+  if (!is.null(include)){
+    if(class(include)[1] != "GRanges")
+      stop("'include' must be a GenomicRanges object")
+    for (i in chromosomes){
+      assign(x = i, value = include[seqnames(include)==i])
+    }
+  } else {
+    for (i in chromosomes){
+        assign(x = i,
+               value = GRanges(seqnames = rep(i,2),
+                               ranges = rep(IRanges(start = 1, end = chromosome_sizes$length[chromosome_sizes$chrom==i]),2),
+                               strand = strands))
+    }
+  }
+
+
+  if (!is.null(sample)){
+    if (!is.numeric(sample))
+      stop("'sample' must be numeric")
+    if (sample<=0)
+      stop("'sample' must be a positive number")
+    if (sample<1){
+      sample <- length(x = alignments)*sample
+    }
+    alignments <- sample(x = alignments, size = sample, replace = FALSE)
+  }
+  if (stranded==FALSE)
+    strand_probabilities <- c(sum(as.numeric(strand(two_mismatches_filtered)=="+")), sum(as.numeric(strand(two_mismatches_filtered)=="-")))
+  n <- length(alignments)
+  results <- GRanges()
+  for (i in 1:n){
+    print(i)
+    row <- alignments[i]
+    if (chrom==FALSE){
+      new_chrom <- as.character(sample(x = as.character(chromosome_sizes$chrom), size = 1, prob = chromosome_sizes$length))
+    } else {
+      new_chrom <- as.character(seqnames(row))
+    }
+    if (stranded==FALSE){
+      new_strand <- sample(x = strands, size = 1, prob = strand_probabilities)
+    } else {
+      new_strand <- as.character(strand(row))
+    }
+    width <- qwidth(alignments[i])
+    #eval(as.name(new_chrom))
+    intervals <- eval(as.name(new_chrom))[as.character(strand(eval(as.name(new_chrom))))==new_strand]
+    new_interval <- sample(intervals, size = 1)
+    tries <- 0
+    repeat
+    {
+      new_interval <- sample(intervals, size = 1)
+      if ((end(new_interval)-start(new_interval))>=width)
+        break
+      tries <- tries+1
+      if (tries == 50)
+        break
+    }
+    if (tries == 50)
+      next
+    new_start <- sample(x = start(new_interval):(end(new_interval)-width), size = 1)
+    results <- c(results, makeGRangesFromDataFrame(data.frame(seqnames=new_chrom, start=new_start, end=new_start+width, strand=new_strand)))
+    #a <- c(a,makeGRangesFromDataFrame(data.frame(seqnames="IV", start="438482", end="594998", strand="-")))
+  }
+  return(sort(sortSeqlevels(x = results)))
+}
+
