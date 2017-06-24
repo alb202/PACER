b1544572e5068d2981b77a086566cdfb82cdff1a
Andrew Blumenfeld <ablumenf@gmail.com>
2017-06-23 18:16:10 -0600

    The sequence function is working, but more testing needed

--- R/main.R
+++ R/main.R
@@ -8,6 +8,7 @@ source("R/alignments.R")
 source("R/filters.R")
 source("R/load.R")
 source("R/other.R")
+source("R/sequences.R")
 
 
 #biocLite("SRAdb")
@@ -97,6 +98,7 @@ gene_intervals <- filter_RNA_from_intervals(gene_intervals)
 exon_intervals <- get_exons_from_biomart(mart = mart)
 exon_intervals <- filter_by_metadata(target = exon_intervals, source = gene_intervals, column = "ensembl_gene_id")
 genome_sequence <- Biostrings::readDNAStringSet(filepath = paste("genomes", genome, genome_files$WBcel235[1], sep = "/"), format = "fasta", use.names = TRUE)
+names(genome_sequence) <- unlist(lapply(strsplit(x = names(genome_sequence), split = " "), FUN = '[[', 1))
 
 two_mismatches <- load_alignments(path = "output/WT_early_rep1_full/two_mm_SRR5023999.bam")
 print(length(two_mismatches))
--- R/sequences.R
+++ R/sequences.R
@@ -1,24 +1,71 @@
-get_DNA_sequence <- function(genome, gr, start, end, type=c("5", "3")){
+get_DNA_sequence <- function(genome, gr, start, end, type=c("5", "3", "full"), name){
 
-  if (start>end)
-    stop("The start position must be prior to the end position")
+  if (start< (-1) | end< (-1))
+    stop("The start and end values must be integers -1 or greater. They indicate how many additional bases should be included on each end.")
 
   plus <- gr[strand(gr)=="+"]
   minus <- gr[strand(gr)=="-"]
-
   # process each
+  if (type=="5"){
+    plus_start <- start(plus)-start     #
+    plus_end <- start(plus)+end         #
+    minus_start <- end(minus)-end      #
+    minus_end <- end(minus)+start      #
+  } else if (type=="3"){
+    plus_start <- end(plus)-start       #
+    plus_end <- end(plus)+end           #
+    minus_start <- start(minus)-end    #
+    minus_end <- start(minus)+start        #
+  } else if (type=="full"){
+    plus_start <- start(plus)-start     #
+    plus_end <- end(plus)+end           #
+    minus_start <- start(minus)-end    #
+    minus_end <- end(minus)+start          #
+  }
+  # print(plus)
+  # print(data.frame(seqnames(plus), plus_start, plus_end, strand(plus), stringsAsFactors = FALSE))
+  # print(length(as.character(seqnames(plus))))
+  # print(length(as.numeric(plus_start)))
+  # print(length(as.numeric(plus_end)))
+  # print(length(as.character(strand(plus))))
+
+
 
   # mapply each
-
+  if (length(plus) > 0){
+    #mcols(plus)[name] <- mapply(FUN = get_sequence_from_genome, as.character(seqnames(plus)), as.numeric(plus_start), as.numeric(plus_end), as.character(strand(plus)), MoreArgs = list(sequences = genome))
+    mcols(plus)[name] <- unlist(unname(lapply(FUN = as.character, X = mapply(FUN = get_sequence_from_genome, as.character(seqnames(plus)),
+                                           as.numeric(plus_start), as.numeric(plus_end), as.character(strand(plus)), MoreArgs = list(sequences = genome)))))
+  }
+  if (length(minus) > 0){
+    #mcols(minus)[name] <- mapply(FUN = get_sequence_from_genome, as.character(seqnames(minus)), as.numeric(minus_start), as.numeric(minus_end), as.character(strand(minus)), MoreArgs = list(sequences = genome))
+    mcols(minus)[name] <- unlist(unname(lapply(FUN = as.character, X = mapply(FUN = get_sequence_from_genome, as.character(seqnames(minus)),
+                                           as.numeric(minus_start), as.numeric(minus_end), as.character(strand(minus)), MoreArgs = list(sequences = genome)))))
+  }
+  # print(class(a))
+  # print(a)
+  # print(class(b))
+  # print(b)
   # combine the results
-
-  # return a new GR with the sequences as metadata? or just the data as a vector? I think you need to connect it so you can do later analysis
+  results <- c(plus,minus)
+  #return(sort(sortSeqlevels(results)))
+  return(sort.GenomicRanges(results))
   }
 
+get_sequence_from_genome <- function(chromosome, start, end, strand, sequences){
+  # print(paste(chromosome, start, end, strand))
+  # print(sequences)
+  result <- DNAString(sequences[[chromosome]][start:end])
+  if (strand=="-")
+    result <- reverseComplement(result)
+  # print(class(result))
+  # print(class(result[1]))
+  # print(result)
+  # print(result[1])
+  # print(class(result[[1]]))
+  return(result)
+}
 
 # One function for the mapply function
 # split the plus and minus strand reads and operate on them separately
 
-get_sequence_from_genome <- function(chromosome, start, end){
-
-}
