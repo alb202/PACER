2e4d981cd770b68a910c4787f4757ff4a7f7712e
Andrew Blumenfeld <ablumenf@gmail.com>
2017-07-15 02:27:03 -0600

    Offset calculations now ignores the chromosome. Antisense works to find 5' to opposite 3'

--- R/calculations.R
+++ R/calculations.R
@@ -55,12 +55,15 @@ count_overlaps_by_width_and_base <- function(gr, regions, alignment_width, base_
 
 calculate_offsets <- function(gr, primary_length, overlap_type="sense", maximum_offset=10){
   # If the type is 'sense', the 5' ends of reads from the same strands are compared
-  if(overlap_type=="sense")
+  if(overlap_type=="sense"){
     strands <- list(c("+", "+"), c("-", "-"))
+    ignore_strand = FALSE
+  }
   # If the type is 'antisense', the 5' end of the primary set is compared to the 3' end of the secondary set
-  if(overlap_type=="antisense")
+  if(overlap_type=="antisense"){
     strands <- list(c("+", "-"), c("-", "+"))
-
+    ignore_strand = TRUE
+  }
   # Make the 4 sets of GenomicRanges
   set1_1 <- granges(gr[width(gr)==primary_length & strand(gr)==strands[[1]][1]])
   set1_2 <- granges(gr[width(gr)!=primary_length & strand(gr)==strands[[1]][2]])
@@ -68,8 +71,8 @@ calculate_offsets <- function(gr, primary_length, overlap_type="sense", maximum_
   set2_2 <- granges(gr[width(gr)!=primary_length & strand(gr)==strands[[2]][2]])
 
   # Find the overlaps that are within the maximum offset
-  set1 <- GenomicRanges::findOverlaps(query = set1_1, subject = set1_2, maxgap = maximum_offset, type = "start", select = "all")
-  set2 <- GenomicRanges::findOverlaps(query = set2_1, subject = set2_2, maxgap = maximum_offset, type = "end", select = "all")
+  set1 <- GenomicRanges::findOverlaps(query = set1_1, subject = set1_2, maxgap = maximum_offset, type = "start", select = "all", ignore.strand = ignore_strand)
+  set2 <- GenomicRanges::findOverlaps(query = set2_1, subject = set2_2, maxgap = maximum_offset, type = "end", select = "all", ignore.strand = ignore_strand)
 
   # Use the 'hits' lists to make list of the GenomicRanges
   pos_query_grs <- set1_1[queryHits(set1)]
@@ -80,17 +83,23 @@ calculate_offsets <- function(gr, primary_length, overlap_type="sense", maximum_
   # Create a data frame with the values from each strand, and bind them into a single data frame
   total_results <- rbind.data.frame(data.frame("offsets" = start(pos_subject_grs) - start(pos_query_grs),
                                                "widths"= width(pos_subject_grs),
-                                               "chromosomes"= seqnames(pos_subject_grs),
-                                               "strands"= strand(pos_subject_grs)),
+                                               #"chromosomes"= seqnames(pos_subject_grs),
+                                               "strands"= strand(pos_query_grs)),
                                     data.frame("offsets" = end(neg_query_grs) - end(neg_subject_grs),
                                                "widths"= width(neg_subject_grs),
-                                               "chromosomes"= seqnames(neg_subject_grs),
-                                               "strands"= strand(neg_subject_grs)))
+                                               #"chromosomes"= seqnames(neg_subject_grs),
+                                               "strands"= strand(neg_query_grs)))
 
   # Count the unique rows and return a data table
-  res_1 <- data.table(dplyr::count(x = total_results, offsets, widths, chromosomes, strands, sort = TRUE))
-  res_2 <- data.table(dplyr::count(x = total_results, widths, chromosomes, strands, sort = TRUE))
-  results <- dplyr::left_join(x = res_1, y = res_2, by = c("widths", "chromosomes", "strands"), copy = TRUE, suffix = c(".x", ".y"))
+  # res_1 <- data.table(dplyr::count(x = total_results, offsets, widths, chromosomes, strands, sort = TRUE))
+  # res_2 <- data.table(dplyr::count(x = total_results, widths, chromosomes, strands, sort = TRUE))
+  # results <- dplyr::left_join(x = res_1, y = res_2, by = c("widths", "chromosomes", "strands"), copy = TRUE, suffix = c(".x", ".y"))
+
+  res_1 <- data.table(dplyr::count(x = total_results, offsets, widths, strands, sort = TRUE))
+  res_2 <- data.table(dplyr::count(x = total_results, widths, strands, sort = TRUE))
+  results <- dplyr::left_join(x = res_1, y = res_2, by = c("widths", "strands"), copy = TRUE, suffix = c(".x", ".y"))
+
+
   results["ratio"] <- results$n.x / results$n.y
   return(results)
 }
