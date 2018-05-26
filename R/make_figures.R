dd67b02faafd18b289b3ac0a9e172d2e4e2a6780
ab <ablumenf@gmail.com>
2018-05-26 10:41:33 -0400

    Moved the "save plots" function to the figures.R file

--- R/make_figures.R
+++ R/make_figures.R
@@ -3,15 +3,15 @@ source("../R/figures.R")
 ##########################################################################################
 ### The main workflow ###
 
-## Make plots and save
-save_plot <- function(p, path, label){
-  print('ggsaving the plot')
-  ggsave(filename = paste(label, ".svg", sep = ""),
-         plot = p,
-         device = "svg",
-         path = path)
-  return(p)
-}
+# ## Make plots and save
+# save_plot <- function(p, path, label){
+#   print('ggsaving the plot')
+#   ggsave(filename = paste(label, ".svg", sep = ""),
+#          plot = p,
+#          device = "svg",
+#          path = path)
+#   return(p)
+# }
 
 
 
